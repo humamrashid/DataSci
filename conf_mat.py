cf708899cd0cf5d66c9ed7c997de3d1b113ccd15
Humam Rashid <humamrashid@gmail.com>
2020-09-17 17:12:58 -0400

    Incremental.

--- conf_mat.py
+++ conf_mat.py
@@ -3,20 +3,19 @@
 # Humam Rashid
 # CISC 7700X, Prof. Sverdlov
 
-import csv
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
-filename = "hw3.data1.csv"
-
-fields = []
-rows = []
-actual = []
 
 def main():
+    actual = []
     dataset = np.loadtxt(open("hw3.data1.csv", "rb"), delimiter=",", skiprows=1)
-    print((dataset[:,-1]))
+
+    # Actual values
+    actual = dataset[:, -1]
+
+    print(actual)
 
 if __name__ == '__main__':
     main()
