1a7baebef72b725b5bd89262174a7568f8635777
Humam Rashid <humamrashid@gmail.com>
2020-09-18 02:42:23 -0400

    Inc.

--- conf_mat.py
+++ conf_mat.py
@@ -3,6 +3,7 @@
 # Humam Rashid
 # CISC 7700X, Prof. Sverdlov
 
+import sys
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
@@ -15,15 +16,16 @@ def main():
     # Assigned values
     assigned = []
 
-    for row in dataset[:20]:
+    for row in dataset:
         y = (24 * row[0]) + (-15 * row[1]) + (-38 * row[2]) + (-7 * row[3]) + (-41 * row[4]) \
                 + (35 * row[5]) + (0 * row[6]) + (-2 * row[7]) + (19 * row[8]) + (33 * row[9]) \
                 + (-3 * row[10]) + (7 * row[11]) + (3 * row[12]) + (-47 * row[13]) \
                 + (26 * row[14]) + (10 * row[15]) + (40 * row[16]) + (-1 * row[17]) \
                 + (3 * row[18]) + (0 * row[19]) + (-6)
-        assigned.append(1) if y < 0 else assigned.append(-1)
+        assigned.append(1) if y > 0 else assigned.append(-1)
 
-    print(assigned)
+    print(len(actual))
+    print(len(assigned))
 
 if __name__ == '__main__':
     main()
