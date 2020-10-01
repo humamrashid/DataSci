b6c385c9989a8144c5218066d976952110a20db7
Humam Rashid <humamrashid@gmail.com>
2020-10-01 15:51:04 -0400

    Added initial outline for stock model.

---
+++ stock_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Humam Rashid
+# CISC 7700X, Prof. Sverdlov
+
+import sys
+import numpy as np
+
+def lin_model():
+    return
+def log_model():
+    return
+def exp_model():
+    return
+def pow_model():
+    return
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
+# EOF.
