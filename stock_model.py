24adc72a78d31e18f72a5203875f2c835f2eb696
Humam Rashid <humamrashid@gmail.com>
2020-10-01 16:10:17 -0400

    Incremental.

--- stock_model.py
+++ stock_model.py
@@ -16,6 +16,42 @@ def pow_model():
     return
 
 def main():
+    ibm_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    msft_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    aapl_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    goog_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    fb_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    pg_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    ge_metrics = {
+            'rev': (1, 2, 3),
+            'ern': (1, 2, 3),
+            'div': (1, 2, 3)
+            }
+    print(ibm_metrics)
     return
 
 if __name__ == '__main__':
