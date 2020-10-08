638d6f56cf44d077b52c8e29cef838f187b2443e
Humam Rashid <humamrashid@gmail.com>
2020-10-08 15:58:50 -0400

    Added next quarter values into metric.

--- stock_models.py
+++ stock_models.py
@@ -94,7 +94,7 @@ def r_squared(Y, P):
         squares.append((i - mean_observed) ** 2)
     total_sum_sq = sum(squares)
     squares = []
-    for i in range(Y.size)
+    for i in range(Y.size):
         squares.append((Y[i] - P[i]) ** 2)
     sum_sq_residuals = sum(squares)
     return (1 - (sum_sq_residuals / total_sum_sq))
@@ -119,18 +119,18 @@ def print_metrics(m):
             \n"""
     print(s)
 
-def load_preds(m):
+def load_preds(m, start, end):
     m['rev_next'] = {
-            'lin': lin_model(m['rev_past']),
-            'log': log_model(m['rev_past']),
-            'exp': exp_model(m['rev_past']),
-            'pow': pow_model(m['rev_past'])
+            'lin': lin_model(m['rev_past'][start:end]),
+            'log': log_model(m['rev_past'][start:end]),
+            'exp': exp_model(m['rev_past'][start:end]),
+            'pow': pow_model(m['rev_past'][start:end])
             }
     m['ern_next'] = {
-            'lin': lin_model(m['ern_past']),
-            'log': log_model(m['ern_past']),
-            'exp': exp_model(m['ern_past']),
-            'pow': pow_model(m['ern_past'])
+            'lin': lin_model(m['ern_past'][start:end]),
+            'log': log_model(m['ern_past'][start:end]),
+            'exp': exp_model(m['ern_past'][start:end]),
+            'pow': pow_model(m['ern_past'][start:end])
             }
     if m['div_past'] is None:
         m['div_next'] = {
@@ -141,80 +141,80 @@ def load_preds(m):
                 }
     else:
         m['div_next'] = {
-                'lin': lin_model(m['div_past']),
-                'log': log_model(m['div_past']),
-                'exp': exp_model(m['div_past']),
-                'pow': pow_model(m['div_past'])
+                'lin': lin_model(m['div_past'][start:end]),
+                'log': log_model(m['div_past'][start:end]),
+                'exp': exp_model(m['div_past'][start:end]),
+                'pow': pow_model(m['div_past'][start:end])
                 }
 
 def main():
     ############################################################################
     #
-    # Past 8 Quarters Revenue, Earnings & Dividends
+    # Past 9 Quarters Revenue, Earnings & Dividends.
     #
-    # (Numbers used are from (Q2, 2018) to (Q1, 2020), inclusive)
+    # (Numbers used are from (Q2, 2018) to (Q2, 2020), inclusive)
     #
     ############################################################################
     ibm_metrics = {
             'sym': 'IBM',
             'rev_past': np.array((20003.00, 18756.00, 21760.00, 18182.00, \
-                    19161.00, 18028.00, 21776.00, 17571.00)),
+                    19161.00, 18028.00, 21776.00, 17571.00, 18123.00)),
             'ern_past': np.array((2.61, 2.94, 2.15, 1.78, 2.81, 1.87, 4.11, \
-                    1.31)),
+                    1.31, 1.52)),
             'div_past': np.array((1.57, 1.57, 1.55, 1.57, 1.62, 1.62, 1.62, \
-                    1.62))
+                    1.62, 1.63))
             }
     msft_metrics = {
             'sym': 'MSFT',
             'rev_past': np.array((30085.00, 29084.00, 32471.00, 30571.00, \
-                    33717.00, 33055.00, 36906.00, 35021.00)),
+                    33717.00, 33055.00, 36906.00, 35021.00, 38033.00)),
             'ern_past': np.array((1.14, 1.14, 1.08, 1.14, 1.71, 1.38, 1.51, \
-                    1.40)),
+                    1.40, 1.46)),
             'div_past': np.array((0.42, 0.42, 0.46, 0.46, 0.46, 0.46, 0.51, \
-                    0.51))
+                    0.51, 0.51))
             }
     aapl_metrics = {
             'sym': 'AAPL',
             'rev_past': np.array((53265.00, 62900.00, 84310.00, 58015.00, \
-                    53809.00, 64040.00, 91819.00, 58313.00)),
+                    53809.00, 64040.00, 91819.00, 58313.00, 59685.00)),
             'ern_past': np.array((0.59, 0.73, 1.05, 0.62, 0.55, 0.76, 1.25, \
-                    0.64)),
+                    0.64, 0.65)),
             'div_past': np.array((0.19, 0.18, 0.19, 0.18, 0.20, 0.19, 0.20, \
-                    0.19))
+                    0.19, 0.21))
             }
     goog_metrics = {
             'sym': 'GOOG',
             'rev_past': np.array((32657.00, 33740.00, 39276.00, 36339.00, \
-                    38944.00, 40499.00, 46075.00, 41159.00)),
+                    38944.00, 40499.00, 46075.00, 41159.00, 38297.00)),
             'ern_past': np.array((4.54, 13.06, 12.77, 9.50, 13.21, 10.12, \
-                    15.35, 9.87)),
+                    15.35, 9.87, 10.13)),
             'div_past': None
             }
     fb_metrics = {
             'sym': 'FB',
             'rev_past': np.array((13231.00, 13727.00, 16914.00, 15077.00, \
-                    16886.00, 17652.00, 21082.00, 17737.00)),
+                    16886.00, 17652.00, 21082.00, 17737.00, 18687.00)),
             'ern_past': np.array((1.74, 1.76, 2.38, 0.85, 0.91, 2.12, 2.56, \
-                    1.71)),
+                    1.71, 1.80)),
             'div_past': None
             }
     pg_metrics = {
             'sym': 'PG',
             'rev_past': np.array((16503.00, 16690.00, 17438.00, 16462.00, \
-                    17094.00, 17798.00, 18240.00, 17214.00)),
+                    17094.00, 17798.00, 18240.00, 17214.00, 17698.00)),
             'ern_past': np.array((0.72, 1.22, 1.22, 1.04, -2.12, 1.36, 1.41, \
-                    1.12)),
+                    1.12, 1.07)),
             'div_past': np.array((0.74, 0.74, 0.74, 0.74, 0.77, 0.77, 0.77, \
-                    0.77))
+                    0.77, 0.82))
             }
     ge_metrics = {
             'sym': 'GE',
             'rev_past': np.array((29162.00, 23392.00, 16670.00, 22202.00, \
-                    23414.00, 23360.00, 26238.00, 20524.00)),
+                    23414.00, 23360.00, 26238.00, 20524.00, 17750.00)),
             'ern_past': np.array((0.07, -2.62, 0.07, 0.41, -0.01, -1.08, 0.06, \
-                    0.70)),
+                    0.70, -0.26)),
             'div_past': np.array((0.14, 0.12, 0.14, 0.01, 0.03, 0.01, 0.03, \
-                    0.01))
+                    0.01, 0.03))
             }
     metrics = [ibm_metrics, msft_metrics, aapl_metrics, goog_metrics, \
             fb_metrics, pg_metrics, ge_metrics]
@@ -222,11 +222,13 @@ def main():
     #
     # Next Quarter Revenue, Earnings & Dividends (Predicted)
     #
+    # (Based on the first 8 oberved values)
+    #
     ############################################################################
     for m in metrics:
-       load_preds(m)
-    print("Next Quarter Predictions (In Millions USD)\
-            \n------------------------------------------\n")
+       load_preds(m, 0, 8)
+    #print("Next Quarter Predictions (In Millions USD)\
+    #       \n------------------------------------------\n")
     for m in metrics:
        print_metrics(m)
 
