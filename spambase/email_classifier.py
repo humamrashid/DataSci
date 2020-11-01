6b92ec2c9780cea2658de90e35735e6283691949
Humam Rashid <humamrashid@gmail.com>
2020-11-01 04:42:56 -0500

    Incremental.

--- spambase/email_classifier.py
+++ spambase/email_classifier.py
@@ -10,7 +10,7 @@ def exit_err(msg, err):
     print(msg, file=sys.stderr)
     sys.exit(err)
 
-def get_pcat(dataset, num_inst, label_pos):
+def get_prior(dataset, num_inst, label_pos):
     prior = {}
     count_spam = 0
     count_ham = 0
@@ -24,15 +24,15 @@ def get_pcat(dataset, num_inst, label_pos):
     prior['ham'] = count_ham / num_inst
     return prior
 
-def get_pwordinclass(dataset, num_inst, num_words, label_pos):
+def get_attrtable(dataset, num_inst, num_attrs, label_pos):
     table = {}
-    table['spam'] = np.empty(num_words, dtype='float64')
-    table['ham'] = np.empty(num_words, dtype='float64')
-    occ_total = np.zeros(num_words, dtype='int64')
-    occ_spam = np.zeros(num_words, dtype='int64')
-    occ_ham = np.zeros(num_words, dtype='int64')
+    table['spam'] = np.empty(num_attrs, dtype='float64')
+    table['ham'] = np.empty(num_attrs, dtype='float64')
+    occ_total = np.zeros(num_attrs, dtype='int64')
+    occ_spam = np.zeros(num_attrs, dtype='int64')
+    occ_ham = np.zeros(num_attrs, dtype='int64')
     for i in range(num_inst):
-        for j in range(num_words):
+        for j in range(num_attrs):
             if dataset[i][j] != 0.0:
                 # Word occurs
                 occ_total[j] += 1
@@ -42,7 +42,7 @@ def get_pwordinclass(dataset, num_inst, num_words, label_pos):
                 elif dataset[i][label_pos - 1] == 0.0:
                     # Word is not spam
                     occ_ham[j] += 1
-    for k in range(num_words):
+    for k in range(num_attrs):
         if occ_total[k] != 0:
             table['spam'][k] = occ_spam[k] / occ_total[k]
             table['ham'][k] = occ_ham[k] / occ_total[k]
@@ -51,26 +51,26 @@ def get_pwordinclass(dataset, num_inst, num_words, label_pos):
             table['ham'][k] = 0.0
     return table
 
-def main(filename, num_inst, num_words, label_pos):
+def main(filename, num_inst, num_attrs, label_pos):
     dataset = np.loadtxt(open(filename, "rb"), dtype='float64', delimiter=",")
     # prior[spam|ham]
-    prior = get_pcat(dataset, num_inst, label_pos)
+    prior = get_prior(dataset, num_inst, label_pos)
     # table[spam|ham][word]
-    table = get_pwordinclass(dataset, num_inst, num_words, label_pos)
+    table = get_attrtable(dataset, num_inst, num_attrs, label_pos)
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        exit_err(f'Usage: {sys.argv[0]} <file_name> <num_inst> <num_words>' \
+        exit_err(f'Usage: {sys.argv[0]} <file_name> <num_inst> <num_attrs>' \
                 + ' <label_pos>', 1)
     num_inst = int(sys.argv[2])
     if num_inst <= 0:
         exit_err('Number of instances must be > 0', 1)
-    num_words = int(sys.argv[3])
-    if num_words <= 0:
+    num_attrs = int(sys.argv[3])
+    if num_attrs <= 0:
         exit_err('Number of words must be > 0', 1)
     label_pos = int(sys.argv[4])
     if label_pos < 0:
         exit_err('Label position must be >= 0', 1)
-    main(sys.argv[1], num_inst, num_words, label_pos)
+    main(sys.argv[1], num_inst, num_attrs, label_pos)
 
 # EOF.
