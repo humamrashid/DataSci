8a8e88f129a123c6409463b8899895c3dae20e29
Humam Rashid <humamrashid@gmail.com>
2020-11-06 16:30:21 -0500

    Inc.

--- kNN_classifier.rb
+++ kNN_classifier.rb
@@ -7,17 +7,19 @@ require 'csv'
 
 # Calculate Euclidean distance
 def calc_dist(a, b)
+  Math::sqrt(a[0...-1].zip(b).inject(0) { |sum, e| sum += (e[0] - e[1])**2 })
 end
 
 # Load the training and test instances into two arrays
 def load_data(file, part, train, test)
-  CSV.foreach(file) {|r| Random.new.rand(1.0) < part ? train << r : test << r}
+  CSV.foreach(file) { |r| Random.new.rand(1.0) < part ? train << r : test << r }
 end
 
 def main
   abort "Usage: #{$PROGRAM_NAME} <file> <partition>" if ARGV.length != 2
   train = []; test = []
   load_data(ARGV[0], ARGV[1].to_f, train, test)
+  puts calc_dist([1,2,3], [4,5,6])
 end
 
 main
