6fa514e20889dc57aa3571d557a89f506eb69053
Humam Rashid <humamrashid@gmail.com>
2020-11-06 04:37:54 -0500

    Inc.

--- kNN_classifier.rb
+++ kNN_classifier.rb
@@ -5,11 +5,9 @@
 
 require 'csv'
 
-test = []
-train = []
-
-abort "Usage: #{$PROGRAM_NAME} <data_file>" if ARGV.length != 1
 def load_data
+  train = []
+  test = []
   CSV.foreach(ARGV[0]) do |row|
     if Random.new.rand(1.0) < 0.8
       train.push(row)
@@ -17,6 +15,10 @@ def load_data
       test.push(row)
     end
   end
+  return train, test
 end
+
+abort "Usage: #{$PROGRAM_NAME} <data_file>" if ARGV.length != 1
+train, test = load_data
 
 # EOF.
