58b46b6d95c38800dfb46925160606b0d742f2bc
Humam Rashid <humamrashid@gmail.com>
2020-11-06 14:14:29 -0500

    Inc.

--- kNN_classifier.rb
+++ kNN_classifier.rb
@@ -10,12 +10,16 @@ def calc_dist(a, b)
 end
 
 # Load the training and test instances into two arrays
-def load_data(file, partition, train, test)
-  r = Random.new.rand(1.0)
-  CSV.foreach(file) {|row| r < partition ? train << row : test << row}
+def load_data(file, part, train, test)
+  CSV.foreach(file) {|r| Random.new.rand(1.0) < part ? train << r : test << r}
 end
 
-abort "Usage: #{$PROGRAM_NAME} <data_file> <partition>" if ARGV.length != 2
-load_data(ARGV[0], partition, train, test)
+def main
+  abort "Usage: #{$PROGRAM_NAME} <file> <partition>" if ARGV.length != 2
+  train = []; test = []
+  load_data(ARGV[0], ARGV[1].to_f, train, test)
+end
+
+main
 
 # EOF.
