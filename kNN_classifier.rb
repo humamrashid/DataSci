14d71ffdc5f65ac5b5e4f2ce5d471168cdffe6fe
Humam Rashid <humamrashid@gmail.com>
2020-11-06 22:22:04 -0500

    Inc.

--- kNN_classifier.rb
+++ kNN_classifier.rb
@@ -10,19 +10,13 @@ def calc_dist(a, b)
   Math::sqrt(a[0...-1].zip(b).inject(0) { |sum, e| sum += (e[0] - e[1])**2 })
 end
 
-def load_data(file, part, train, test)
-  train = []; test = []
-  CSV.foreach(file) do |row|
-    rf = row.map(&:to_f)
-    Random.new.rand(1.0) < part ? train << rf : test << rf
-  end
-  return train, test
-end
-
 abort "Usage: #{$PROGRAM_NAME} <file> <partition>" if ARGV.length != 2
 train = []; test = []
 until train.length > 0 and test.length > 0 do
-  train, test = load_data(ARGV[0], ARGV[1].to_f, train, test)
+  CSV.foreach(ARGV[0]) do |row|
+    rf = row.map(&:to_f)
+    Random.new.rand(1.0) < ARGV[1].to_f ? train << rf : test << rf
+  end
 end
 test.each do |r|
   min_dist = calc_dist(r, train[min_index = 0])
