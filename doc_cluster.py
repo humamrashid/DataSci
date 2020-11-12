75096e786e45c05892709958fcf48a2308fe90c1
Humam Rashid <humamrashid@gmail.com>
2020-11-12 17:53:31 -0500

    WIP: document clustering.

---
+++ doc_cluster.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Document clustering.
+
+# Humam Rashid
+# CISC 7700X, Prof. Sverdlov
+
+def get_dist(a, b):
+    return
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
+
+# EOF.
