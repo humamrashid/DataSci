9f82c75b8029121f80f79b1e98e8d3c1327a512f
Humam Rashid <humamrashid@gmail.com>
2020-11-13 04:44:47 -0500

    Inc.

--- doc_cluster.py
+++ doc_cluster.py
@@ -5,6 +5,9 @@
 # CISC 7700X, Prof. Sverdlov
 
 import math
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Return the Euclidean distance between a and b.
 def calc_dist(a, b):
@@ -13,8 +16,29 @@ def calc_dist(a, b):
         s += (e[0] - e[1])**2
     return math.sqrt(s)
 
+def count_vec(text):
+    vectorizer = CountVectorizer()
+    vectorizer.fit(text)
+    print(vectorizer.vocabulary_)
+    vector = vectorizer.transform(text)
+    print(vector.shape)
+    print(type(vector))
+    print(vector.toarray())
+
+def tfidf_vec(text):
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(text)
+    print(vectorizer.vocabulary_)
+    print(vectorizer.idf_)
+    vector = vectorizer.transform([text[0]])
+    print(vector.shape)
+    print(vector.toarray())
+
 def main():
-    return
+    text = ["The quick brown fox jumped over the lazy dog.",
+            "the dog",
+            "the fox"]
+    tfidf_vec(text)
 
 if __name__ == "__main__":
     main()
